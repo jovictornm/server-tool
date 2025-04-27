265828841a2957966e1e0897681aa13630d84f11
jovictornm <81192050+jovictornm@users.noreply.github.com>
2025-04-27 17:55:48 -0300

    Delete src directory

--- src/debian_ubuntu/1_1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo Initializing Web Server instalattion on Debian/Ubuntu distro!
-# Detecta a distribuição Linux
-#DISTRO=$(lsb_release -i | awk '{print $3}')
-
-read -p "root password: " root_password
-echo root_password
-
